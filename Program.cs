28d094c5f6f08b6bf9a01619bcb00f616b04dc56
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-26 14:36:07 +0300

    добавил второй способ решения

--- Program.cs
+++ Program.cs
@@ -1,14 +1,22 @@
-﻿int method(int A)
-{
-    int i = 1;
-    int summ = 0;
-    while (i <= A)
-    {
-        summ = summ + i;
-        i++;
-    }
-    return summ;
-}
+﻿//int method(int A)
+//{
+//    int i = 1;
+//    int summ = 0;
+//    while (i <= A)
+//    {
+//        summ = summ + i;
+//        i++;
+//    }
+//    return summ;
+//}
 
-int A = 25876;
-Console.WriteLine(method(A));
+//int A = 25876;
+//Console.WriteLine(method(A));
+
+int A = 9;
+int summ = 0;
+for (int i = 1; i <= A; i++)
+{
+    summ = summ + i;
+}
+Console.WriteLine(summ);
