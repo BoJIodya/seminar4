fc1087b42d59a6cf3f60e4c83a4cfc1e1f66b75c
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-26 14:31:19 +0300

    commit init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int method(int A)
+{
+    int i = 1;
+    int summ = 0;
+    while (i <= A)
+    {
+        summ = summ + i;
+        i++;
+    }
+    return summ;
+}
+
+int A = 25876;
+Console.WriteLine(method(A));
